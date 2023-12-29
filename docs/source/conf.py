1f3460560624c9aa55a9cbb17193cb18871798e0
Mengqi Zhao <mengqiz@umd.edu>
2023-12-28 16:51:29 -0800

    Update documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,16 +5,21 @@
 
 
 # -- Initial Setup -----------------------------------------------------
-# 
+
+import os
+import sys
+
 import sphinx_rtd_theme
+
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'pytemplate'
-copyright = 'JGCRI 2023'
-author = 'JGCRI'
-release = '0.0.0'
+project = 'GLORY'
+copyright = '2021, Battelle Memorial Institute'
+author = 'Mengqi Zhao'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -35,13 +40,44 @@ extensions = [
     'sphinx-mathjax-offline'
 ]
 
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# The suffix of source filenames.
+source_suffix = [".rst"]
 
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
 html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = [
+    "css/glory.css",
+    "css/getting_started.css",
+]
+
+import glory
+version = str(glory.__version__)
+
+
+# -- Extension configuration -------------------------------------------------
+# panels_add_bootstrap_css = False
+
+
+# -- Options for todo extension ----------------------------------------------
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = True
