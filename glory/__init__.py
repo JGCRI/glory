770f9e4d5b388806f4e5cab42f936e774088477b
crvernon <chrisrvernon@gmail.com>
2024-06-27 16:42:55 -0600

    get version from toml file

--- glory/__init__.py
+++ glory/__init__.py
@@ -1,14 +1,16 @@
+import importlib.metadata
+
 from .data.read_config import ConfigReader
 from .data.read_data import DataLoader
 from .data.write_outputs import write_outputs
-
 from .method.supply_curve import SupplyCurve
 from .method.lp import lp_model, lp_solution
-
 from .utils.diagnostics import diagnostics
 from .utils.logger import init_logger
 from .utils.cleanup import cleanup
-from .utils.install_supplement import *
-
+from .utils.install_supplement import get_example_data
 from .model import execute, run_model
-__version__ = "1.0.0"
+
+
+__version__ = importlib.metadata.version(__package__ or __name__)
+
